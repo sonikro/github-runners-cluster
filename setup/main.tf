534d254cd3c5133597a1c25c87bdf719e5781b0c
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2022-05-21 18:26:00 -0500

    fix: k8s setup terraform

--- setup/main.tf
+++ setup/main.tf
@@ -6,6 +6,9 @@ data "aws_eks_cluster_auth" "default" {
   name = var.cluster_name
 }
 
+provider "aws" {
+  region = var.region
+}
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
@@ -20,8 +23,8 @@ provider "helm" {
   }
 }
 
-resource "local_file" "kubeconfig" {
-  sensitive_content = templatefile("${path.module}/kubeconfig.tpl", {
+resource "local_sensitive_file" "kubeconfig" {
+  content = templatefile("${path.module}/kubeconfig.tpl", {
     cluster_name = var.cluster_name,
     clusterca    = data.aws_eks_cluster.default.certificate_authority[0].data,
     endpoint     = data.aws_eks_cluster.default.endpoint,
--- setup/variables.tf
+++ setup/variables.tf
@@ -2,3 +2,7 @@ variable "cluster_name" {
   default     = "sonikro-eks"
   description = "Name of the EKS cluster"
 }
+
+variable "region" {
+  default = "us-east-1"
+}
