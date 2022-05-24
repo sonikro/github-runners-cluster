6b07e72e03d44c965c315aeb6ac2388b72b1925b
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2022-05-23 21:59:37 -0500

    fix: fixed healm installation

--- setup/cert-manager.tf
+++ setup/cert-manager.tf
@@ -1,12 +1,13 @@
 
 resource "helm_release" "cert-manager" {
-  name       = "cert-manager"
-  namespace  = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart      = "jetstack/cert-manager"
-  wait       = true
-  timeout    = 600
-  version    = "1.8.0"
+  create_namespace = true
+  name             = "cert-manager"
+  namespace        = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  wait             = true
+  timeout          = 600
+  version          = "1.8.0"
   set {
     name  = "installCRDs"
     value = "true"
--- setup/dashboard.tf
+++ setup/dashboard.tf
@@ -1,11 +1,9 @@
 resource "helm_release" "dashboard" {
-  namespace = data.kubernetes_namespace.kube-system-ns.metadata.0.name
-  wait      = true
-  timeout   = 600
-
-  name = "k8s-dashboard"
-
-  repository = "https://kubernetes.github.io/dashboard/"
-  chart      = "kubernetes-dashboard/kubernetes-dashboard"
+  namespace  = data.kubernetes_namespace.kube-system-ns.metadata.0.name
+  wait       = true
+  timeout    = 600
+  name       = "k8s-dashboard"
+  repository = "https://kubernetes.github.io/dashboard"
+  chart      = "kubernetes-dashboard"
   version    = "5.4.1"
 }
