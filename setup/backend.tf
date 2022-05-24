2d0ff75aaad5e1e8bbe25691349fa7086c42c976
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2022-05-23 21:47:32 -0500

    feat: implemented cert-manager chart

--- setup/backend.tf
+++ setup/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket               = "sonikro-terraform"
-    key                  = "terraform.tfstate"
+    key                  = "github-runners-cluster-setup-k8s.tfstate"
     workspace_key_prefix = "github-runners-cluster-setup-k8s"
     region               = "us-east-1"
   }
---
+++ setup/cert-manager.tf
@@ -0,0 +1,14 @@
+
+resource "helm_release" "cert-manager" {
+  name       = "cert-manager"
+  namespace  = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "jetstack/cert-manager"
+  wait       = true
+  timeout    = 600
+  version    = "1.8.0"
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
